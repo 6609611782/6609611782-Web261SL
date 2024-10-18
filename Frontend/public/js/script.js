6c03d463b3e22974de4cad4f6cd5df3112d7bf96
Jakarin Sang-ong - 6609611782 <jakarin.san@dome.tu.ac.th>
2024-10-18 17:08:02 +0700

    first commit

---
+++ Frontend/public/js/script.js
@@ -0,0 +1,34 @@
+async function TU_REST_API() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value; 
+  
+    const data = {
+        UserName: username,
+        PassWord: password
+    };
+  
+    try {
+        const applicationKey = "ปปปปปปปปปปปปปปป";
+        const response = await fetch('https://restapi.tu.ac.th/api/v1/auth/Ad/verify', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Application-Key': applicationKey
+            },
+            body: JSON.stringify(data)
+        });
+
+        if (response.ok) {
+            const result = await response.json();
+            console.log('Login Successful:', result);
+            alert('Login success');
+        } else {
+            const errorResult = await response.json();
+            console.log('Login failed:', result);
+            alert('Login failed: ' + result.message);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        alert('An error occurred while logging in.');
+    }
+}
